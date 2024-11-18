c48f74b4d7d28fa3fd7d557ca4779f482efb005b
Pedro Tavares <pedro.tavares@liferay.com>
2024-11-18 20:49:12 -0300

    Criar endpoint para loan (emprestimo)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const sequelize= require('sequelize');
 const authRoute = require('./routes/auth.routes');
 const userRoute = require('./routes/user.routes');
 const bookRoute = require('./routes/book.routes');
+const loanRoute = require('./routes/loan.routes');
 const db = require('./config/database');
 const Address = require('./models/Address');
 const Book = require('./models/Book');
@@ -33,6 +34,7 @@ app.use(express.urlencoded({extended: false}));
 app.use('/api/auth', authRoute);
 app.use('/api/user', userRoute);
 app.use('/api/book', bookRoute);
+app.use('/api/loan', loanRoute);
 
 app.listen(port, ()=>{
   console.log(`server running at port ${port}`);
