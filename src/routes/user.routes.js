aa0bd3480de64f77bf20c09b2641832017dd4479
Marcello Joaquim <marcellojoaquim1@hotmail.com>
2024-11-21 20:19:12 -0300

    alteracoes no servico de login e autenticacao

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user.controller');
+const authToken = require('../middleware/authToken');
 
-router.get('/find', userCtrl.find);
+router.get('/find',[authToken.verifyToken], userCtrl.find);
 router.put('/update/:id', userCtrl.update);
   
 module.exports = router;
